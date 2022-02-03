c075ff451e89210eb3033de1067d2337872cb96a
Hon Ching YEUNG <79874151+honcyeung@users.noreply.github.com>
2022-02-03 20:59:08 +0100

    Create pdf.py

---
+++ pdf.py
@@ -0,0 +1,13 @@
+import PyPDF2
+import sys
+
+# take terminal inputs, eg pdf files
+inputs = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('merged.pdf')
+
+pdf_combiner(inputs)
